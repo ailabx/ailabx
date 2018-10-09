1afa9888a2b3706dc10e9fc7a99e3909020397fd
martinwei1982 <86820609@qq.com>
2018-10-09 15:56:25 +0800

    删除pyqt5-gui

--- quant/gui/forms/formsetting.py
+++
@@ -1,7 +0,0 @@
-from PyQt5 import QtWidgets
-from PyQt5 import QtGui
-
-class FormSetting(QtWidgets.QMainWindow):
-    def __init__(self,parent=None):
-        super(FormSetting,self).__init__(parent)
-        self.setWindowTitle('设置')
