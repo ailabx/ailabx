48af1dc715bd9211913603bc48788b05e9e3ef27
ailabx <86820609@qq.com>
2022-11-27 13:58:41 +0800

    Delete common directory

--- common/bn_utils.py
+++
@@ -1,9 +0,0 @@
-from binance.spot import Spot
-
-client = Spot()
-# Get server timestamp
-print(client.time())
-# Get klines of BTCUSDT at 1m interval
-data = client.klines("BTCUSDT", "1h")
-print(len(data))
-print(data)
